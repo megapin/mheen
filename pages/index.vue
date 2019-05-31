1667eeea5ced57d706660d9d5159e1cf6b57f186
megapin <yeonjh549@gmail.com>
2019-05-31 15:08:22 +0900

    second commit

--- pages/index.vue
+++ pages/index.vue
@@ -14,7 +14,7 @@
         <vuetify-logo />
       </div>
       <v-card>
-        <v-card-title class="headline">Welcome to MHeen Interact!!</v-card-title>
+        <v-card-title class="headline">Welcome to MHeen Interact!!!</v-card-title>
         <v-card-text>
           <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
           <p>For more information on Vuetify, check out the <a
